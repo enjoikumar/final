dbd9fc43f6040899500d53ed03808985c56b31b0
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2016-07-12 10:00:24 -0400

    a new start

--- server.js
+++ server.js
@@ -1,63 +1,29 @@
-// =============
-// requirements
-// =============
+//Dependencies
+//Express
 var express = require('express');
 var app = express();
-var morgan = require("morgan");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
-var cookieParser = require("cookie-parser");
-var ngrok = require('ngrok');
-var port = process.env.PORT || 3000;
+//Morgan
+var morgan = require('morgan');
+//Body-parser
+var bodyParser = require('body-parser');
+//Mongoose
+var mongoose = require('mongoose');
+//Request
+var request = require('request');
 
-
-// =============
-// Middleware
-// =============
-app.use(morgan("dev"));
+//Middleware
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(methodOverride(function(req, res){
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    var method = req.body._method;
-    delete req.body._method;
-    return method;
-  }
-}));
-app.use(cookieParser());
+app.use(express.static('public'));
 
+//Importing Controllers 
+// var imgur = require('./controllers/imgur_controller.js');
+// app.use('/coolpicapp', imgur);
 
-// =============
-// Middleware
-// =============
-app.use(express.static("public"));
-
-
-//==========
-// database
-//==========
-var db = process.env.MONGODB_URI || "mongodb://localhost/final";
-mongoose.connect(db);
-//REMEMBER TO TURN ON MONGOD 
-
-
-//==========
-// Ngrok 
-//==========
-ngrok.connect({
-	proto: 'http', 
-	addr: port,  
-	auth: 'user:pwd', 
-	region: 'us'
-}, function (err, url) {
+//Server execution
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+	console.log("server is running on " + port);
 });
 
-
-// =============
-// Server
-// =============
-app.listen(port);
-console.log("==============");
-console.log("server running on port " + port);
-console.log("==============");
