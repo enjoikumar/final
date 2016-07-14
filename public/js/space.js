b569ad149dfba4aec23b58c337de17112f48446d
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2016-07-14 11:51:58 -0400

    fixed pluto

--- public/js/space.js
+++ public/js/space.js
@@ -4,6 +4,16 @@ $( document ).ready(function() {
 // $('head').append('<style>#planet{-webkit-animation-duration:5s;}</style>');
 
 
+// $('head').append('<style>.mercury{-webkit-animation-duration:5s;}</style>');
+// $('head').append('<style>.venus{-webkit-animation-duration:8s;}</style>');
+// $('head').append('<style>.earth{-webkit-animation-duration:12s;}</style>');
+// $('head').append('<style>.mars{-webkit-animation-duration:20s;}</style>');
+// $('head').append('<style>.jupiter{-webkit-animation-duration:30s;}</style>');
+// $('head').append('<style>.saturn{-webkit-animation-duration:60s;}</style>');
+// $('head').append('<style>.uranus{-webkit-animation-duration:70s;}</style>');
+// $('head').append('<style>.neptune{-webkit-animation-duration:100s;}</style>');
+// $('head').append('<style>.pluto{-webkit-animation-duration:120s;}</style>');
+
 
 
 
