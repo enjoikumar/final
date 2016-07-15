95e2823c68d8ae9e2e3c7cb43a5dd324a54b9d49
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2016-07-15 11:04:02 -0400

    added xyz values

--- public/js/stars.js
+++ public/js/stars.js
@@ -10,13 +10,45 @@ var u = 0;
 var dep = w;
 var dp = 0.70;
 
-var ms = {
+//mouse locations
+var rat = {
+  x: 0,
+  y: 0
+};
+var mouse = {
   x: 0,
   y: 0
 };
 
-var msd = {
-  x: 0,
-  y: 0
+//points of depth and perception
+function Obj(x, y, z) {
+  this.set(x, y, z);
+}
+//setting a value so it always starts at 0
+Obj.prototype = {
+  set: function(x, y, z) {
+    this.x = x || 0;
+    this.y = y || 0;
+    this.z = z || 0;
+  },
+  //this is for rotation of the angle using matrices
+  //making X responsive
+  rotX: function(ang) {
+    var y = this.y;
+    var z = this.z;
+    this.y = y * Math.cos(ang) - z * Math.sin(ang);
+  },
+  // making y responsive
+  rotY: function(ang) {
+    var x = this.x;
+    var z = this.z;
+    this.x = x * Math.cos(ang) - z * Math.sin(ang);
+  },
+  // z responsive
+  rotZ: function(ang) {
+    var x = this.x;
+    var y = this.y;
+    this.x = x * Math.cos(ang) - y * Math.sin(ang);
+  }
 };
 
